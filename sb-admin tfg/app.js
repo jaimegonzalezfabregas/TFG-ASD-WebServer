b9e8c7398c9d7df55449f7267c6285a7bcb8d127
jgaldoo <101014368+jgaldoo@users.noreply.github.com>
2023-11-19 18:59:07 +0100

    Add files via upload

--- sb-admin tfg/app.js
+++ sb-admin tfg/app.js
@@ -1,8 +1,52 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const { time } = require('console');
+const app = express();
+const port = 5500;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("A response")
-})
+  res.sendFile(path.join( __dirname, "/login.html"));
+});
 
-app.listen(5000)
+app.get('/formulario-end.html/', (req, res) => {
+  console.log(req.query);
+  let esp = '';
+  if(Object.keys(req.query).length != 0) {
+    esp = req.query.espacio;
+  }
+  const currentTime = new Date();
+  let currentHour = currentTime.getHours();
+  let currentMinute = currentTime.getMinutes();
+
+  res.render('formulario-end', {espacio: esp, hora: `${currentHour}:${currentMinute}`});
+});
+
+app.post('/login.html', (req, res) => {
+  console.log(`Got a POST in login with ${JSON.stringify(req.body)}`);
+  res.redirect("/index.html");
+});
+
+app.post('/formulario-aulas-qr.html', (req, res) => {
+  console.log(`Got a POST in formulario-aulas-qr with ${JSON.stringify(req.body)}`);
+  res.redirect(`/formulario-end-qr.html`);
+});
+
+app.post('/formulario-aulas.html', (req, res) => {
+  console.log(`Got a POST in formulario-aulas with ${JSON.stringify(req.body)}`);
+  res.redirect(`/formulario-end.html/?espacio=${req.body.espacio}`);
+});
+
+app.post('/formulario-end.html', (req, res) => {
+  console.log(`Got a POST in formulario-end with ${JSON.stringify(req.body)}`);
+  res.redirect(`/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
---
+++ sb-admin tfg/datos.js
@@ -0,0 +1,70 @@
+const users = [
+    {
+        id: "EC",
+        nombre: "Estructura de Computadores",
+        grupo: "B",
+        periodo: "2"
+    },
+    {
+        id: "TOC",
+        nombre: "Tecnología y Organización de Computadores",
+        grupo: "B",
+        periodo: "1"
+    },
+    {
+        id: "AM",
+        nombre: "Ampliación de Matemáticas",
+        grupo: "B",
+        periodo: "1"
+    },
+    {
+        id: "PE",
+        nombre: "Probabilidad y Estadística",
+        grupo: "B",
+        periodo: "2"
+    },
+    {
+        id: "BD",
+        nombre: "Bases de Datos",
+        grupo: "B",
+        periodo: "1"
+    },
+    {
+        id: "ED",
+        nombre: "Estructuras de Datos",
+        grupo: "B",
+        periodo: "2"
+    },
+    {
+        id: "FAL",
+        nombre: "Fundamentos de la Algoritmia",
+        grupo: "B",
+        periodo: "1"
+    },
+    {
+        id: "IS1",
+        nombre: "Ingeniería del Software I",
+        grupo: "B",
+        periodo: "1"
+    },
+    {
+        id: "IS2",
+        nombre: "Ingeniería del Software II",
+        grupo: "B",
+        periodo: "2"
+    },
+    {
+        id: "TP1",
+        nombre: "Tecnología de la Programación I",
+        grupo: "B",
+        periodo: "1"
+    },
+    {
+        id: "TP2",
+        nombre: "Tecnología de la Programación II",
+        grupo: "B",
+        periodo: "2"
+    }
+  ];
+  
+  module.exports = users;
