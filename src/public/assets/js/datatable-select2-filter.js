bfdb5b1a28672f3575f80fda33a288b40b91eb64
jgaldoo <101014368+jgaldoo@users.noreply.github.com>
2024-02-11 23:01:08 +0100

    Add files via upload

---
+++ src/public/assets/js/datatable-select2-filter.js
@@ -0,0 +1,4 @@
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.dt-status-filter').select2();
+});
--- src/public/assets/js/datatables-simple-demo.js
+++ src/public/assets/js/datatables-simple-demo.js
@@ -4,6 +4,16 @@ window.addEventListener('DOMContentLoaded', event => {
 
     const datatablesSimple = document.getElementById('datatablesSimple');
     if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+        new simpleDatatables.DataTable(datatablesSimple, {
+            labels: {
+                placeholder: "Buscar...",
+                searchTitle: "Buscando en la tabla",
+                pageTitle: "Página {page}",
+                perPage: "entradas por página",
+                noRows: "No se han encontrado entradas",
+                info: "Mostrando entradas {start} - {end} de {rows}",
+                noResults: "Ningún resultado encaja con la búsqueda"
+            }
+        });
     }
 });
