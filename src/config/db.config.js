c72a587114b0066f073e8f81e7d808c21ae6fe9f
jgaldoo <101014368+jgaldoo@users.noreply.github.com>
2024-02-11 22:56:40 +0100

    Add files via upload

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS
+    host: process.env.DB_DEV_HOST,
+    port: process.env.DB_DEV_PORT,
+    name: process.env.DB_DEV_NAME,
+    user: process.env.DB_DEV_USER,
+    password: process.env.DB_DEV_PASS
 }
 
