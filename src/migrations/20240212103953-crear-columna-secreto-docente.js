40a759fa5f8cb77a8588c92ad358f7077754ee50
Helenan <100680252+Helenan@users.noreply.github.com>
2024-02-12 12:44:11 +0100

    Add files via upload

---
+++ src/migrations/20240212103953-crear-columna-secreto-docente.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return await queryInterface.addColumn('Docente', 'secret', {
+          type: Sequelize.DataTypes.STRING,
+          allowNull: false
+      }
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return await queryInterface.deleteColumn('Docente', 'secret');
+  }
+};
