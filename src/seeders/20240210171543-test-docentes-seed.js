fc51c50ff1e3847401f517a428e57a6046077f2c
Helenan <100680252+Helenan@users.noreply.github.com>
2024-02-12 12:44:31 +0100

    Add files via upload

--- src/seeders/20240210171543-test-docentes-seed.js
+++ src/seeders/20240210171543-test-docentes-seed.js
@@ -13,9 +13,9 @@ module.exports = {
      * }], {});
     */
     return await queryInterface.bulkInsert('Docente', [
-      {nombre: 'Marta', apellidos: 'Estévez García', email: 'maestga@fdi.ucm.es', password: 'asdf', rol: 'Admin'},
-      {nombre: 'Alejandro', apellidos: 'Ortiz Perseida', email: 'alejop@fdi.ucm.es', password: 'fdsa', rol: 'Decanato'},
-      {nombre: 'Marcelo', apellidos: 'Adilo Orense', email: 'marcador@fdi.ucm.es', password: 'sdfg', rol: 'Usuario'}
+      {nombre: 'Marta', apellidos: 'Estévez García', email: 'maestga@fdi.ucm.es', password: '$2b$04$wLEiuS.XWrJ5.46HVdNSoOHqR9tV3scXUnQIKkq2boTExDh2rWapu', rol: 'Admin', secret: "P4EAE3S2IJBEKYRR"},
+      {nombre: 'Alejandro', apellidos: 'Ortiz Perseida', email: 'alejop@fdi.ucm.es', password: '$2b$04$dh1HMKzSdaOvjoZFpQeN1.qheU.v8B0m.AWY4WkJKejr4NBi6jioS', rol: 'Decanato', secret: "IA7FADD2INHAC4YD"},
+      {nombre: 'Marcelo', apellidos: 'Adilo Orense', email: 'marcador@fdi.ucm.es', password: '$2b$04$CllYtypIyBrkpXALjvBYveFIe7eKUiw.dJwK2Ny4kQTiPDynZpNg2', rol: 'Usuario', secret: "AEZCQ2TXM42BWMSU"}
     ]);
 
   },
