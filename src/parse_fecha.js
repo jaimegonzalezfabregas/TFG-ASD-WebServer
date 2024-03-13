f8ed74996beec85926da008b77a639fc0a400834
Helenan <you@example.com>
2024-03-13 16:18:52 +0100

    Creación de middleware para recurrencias y creación masiva de datos desde csv
    Co-authored-by: jgaldoo <jgaldoo@users.noreply.github.com>

---
+++ src/parse_fecha.js
@@ -0,0 +1,140 @@
+const moment = require('moment');
+const recTypeParser = {
+    Diaria: 'days',
+    Semanal: 'weeks',
+    Mensual: 'months',
+    Anual: 'years'
+}
+
+function getInicioDiferencia(act, rec) {
+    // Parseamos los tiempos de inicio y fin (en horas)
+    let hora_inicio, minuto_inicio, diferencia_horas, diferencia_minutos;
+    if (act.es_todo_el_dia == 'Sí') {
+        hora_inicio = 0;
+        minuto_inicio = 0;
+        diferencia_horas = 24;
+        diferencia_minutos = 0;
+    }
+    else {
+        try {
+            let split_point_ini = act.tiempo_inicio.indexOf(':');
+            hora_inicio = parseInt(act.tiempo_inicio.substring(0, split_point_ini));
+            minuto_inicio = parseInt(act.tiempo_inicio.substring(split_point_ini + 1));
+
+            let split_point_fin = act.tiempo_fin.indexOf(':');
+            let hora_fin = parseInt(act.tiempo_fin.substring(0, split_point_fin));
+            let minuto_fin = parseInt(act.tiempo_fin.substring(split_point_fin + 1));
+            diferencia_horas = hora_fin - hora_inicio;
+            diferencia_minutos = minuto_fin - minuto_inicio;
+        }
+        catch (error) {
+            console.log("Error while parsing tiempo_inicio: ", error);
+            throw error;
+        }
+    }
+
+    return [hora_inicio, minuto_inicio, diferencia_horas, diferencia_minutos];
+}
+
+function primeraRecurrencia(act, rec) {
+    let [hora_inicio, minuto_inicio] = getInicioDiferencia(act, rec);
+
+    // Encontrar el primer evento
+    let inicio = moment(act.fecha_inicio, 'YYYY-MM-DDTHH:mm');
+    
+    // Calibramos las horas y minutos
+    let minuto_partida = inicio.minute();
+    let minutos_hora = 60;
+    let offset_minutos = (minuto_inicio < minuto_partida) ?  minutos_hora - (minuto_partida - minuto_inicio) : minuto_inicio - minuto_partida;
+    inicio.add(offset_minutos, 'minutes');
+
+    let hora_partida = inicio.hour();
+    let horas_dia = 24;
+    let offset_horas = (hora_inicio < hora_partida) ?  horas_dia - (hora_partida - hora_inicio) : hora_inicio - hora_partida;
+    inicio.add(offset_horas, 'hours');
+
+    let dias_semana, dias_mes, meses_anio;
+    let dia_partida, mes_partida, offset_dias, offset_meses;
+    switch (rec.tipo_recurrencia) { // Si la recurrencia es diaria, se puede omitir
+        case 'Semanal':
+            dia_partida = inicio.isoWeekday();
+            dias_semana = 7;
+            offset_dias = (rec.dia_semana < dia_partida) ?  dias_semana - (dia_partida - rec.dia_semana) : rec.dia_semana - dia_partida;
+            inicio.add(offset_dias, 'days');
+            break;
+        case 'Mensual':
+            dia_partida = inicio.date();
+            dias_mes = inicio.daysInMonth();
+            offset_dias = (rec.dia_mes < dia_partida) ? dias_mes - (dia_partida - rec.dia_mes) : rec.dia_mes - dia_partida;
+            inicio.add(offset_dias, 'days');
+            break;
+        case 'Anual':
+            dia_partida = inicio.date();
+            dias_mes = inicio.daysInMonth();
+            offset_dias = (rec.dia_mes < dia_partida) ? dias_mes - (dia_partida - rec.dia_mes) : rec.dia_mes - dia_partida;
+            inicio.add(offset_dias, 'days');
+        
+            mes_partida = inicio.month();
+            meses_anio = 12;
+            let mes_anio_moment = rec.mes_anio - 1; // Para moment, el mes 5 es en realidad Junio, así pues, el 4 será Mayo. Esto se aplica a todos los meses
+            offset_meses = (mes_anio_moment < mes_partida) ? meses_anio - (mes_partida - mes_anio_moment) : mes_anio_moment - mes_partida;
+            inicio.add(offset_meses, 'months')
+            break;
+        default:
+            break;
+    }
+
+    return inicio;
+}
+
+function fechaFromActividadRecurrencia(act, rec) {
+    let punto_fin = (act.fecha_fin) ? moment(act.fecha_fin) : null;
+    let tipo_rec = recTypeParser[rec.tipo_recurrencia];
+    let max_veces = act.maximo || 32; 
+    let separacion = rec.separacion || 0;
+    let [,, diferencia_horas, diferencia_minutos] = getInicioDiferencia(act, rec);
+
+    // Aplicar la recurrencia para sacar el resto de eventos
+    let eventos_resultantes = [];
+    let a_evento = primeraRecurrencia(act, rec);
+    // Si no hay final, 32 veces. Si lo hay, todas las posibles (separado para que no compruebe siempre la misma condición)
+    if (punto_fin == null){
+        for (let i = 0; i < max_veces; i++) {
+            let clon = a_evento.clone().add({hours: diferencia_horas, minutes: diferencia_minutos});
+            eventos_resultantes.push({ inicio: a_evento.toISOString(true), fin: clon.toISOString(true)});
+            a_evento.add(separacion + 1, tipo_rec);
+        }
+    }
+    else {
+        
+        for (let i = 0; a_evento < punto_fin; i++) {
+            let clon = a_evento.clone().add({hours: diferencia_horas, minutes: diferencia_minutos});
+            eventos_resultantes.push({ inicio: a_evento.toISOString(true), fin: clon.toISOString(true)});
+            a_evento.add(separacion + 1, tipo_rec);
+        }
+    }
+    
+    return eventos_resultantes;
+}
+
+function isInRecurrencia(act, rec, fecha) {
+    let inicio = primeraRecurrencia(act, rec);
+    let separacion = rec.separacion;
+    let tipo_rec = recTypeParser[rec.tipo_recurrencia];
+    let a_comparar = moment(fecha, 'YYYY-MM-DDTHH:mm');
+    // Para que esté en la recurrencia, la diferencia entre la primera actividad y esta debe, en la unidad de separación,
+    // debe ser divisible por la separación entre actividades impuesta por la recurrencia
+    return (inicio.diff(a_comparar, tipo_rec) % separacion == 0);
+}
+
+fechaFromActividadRecurrencia({fecha_inicio: "2024-03-07T16:45:11.647", fecha_fin: "2024-06-22T16:45:11.647", tiempo_inicio: '16:00', 
+tiempo_fin: '17:40', es_todo_el_dia: 'No', es_recurrente: 'Sí', creadoPor: 'Galdo', responsable_id: 3},
+{tipo_recurrencia: 'Semanal', dia_semana: 4, dia_mes: 22, mes_anio: 5, separacion: 1});
+
+isInRecurrencia({fecha_inicio: "2024-03-07T16:45:11.647", fecha_fin: "2024-06-22T16:45:11.647", tiempo_inicio: '16:00', 
+tiempo_fin: '17:40', es_todo_el_dia: 'No', es_recurrente: 'Sí', creadoPor: 'Galdo', responsable_id: 3},
+{tipo_recurrencia: 'Semanal', dia_semana: 4, dia_mes: 22, mes_anio: 5, separacion: 1}, "2024-06-22T16:45:11.647");
+
+module.exports = {
+    fechaFromActividadRecurrencia, isInRecurrencia
+}
--- src/parse_horarios.js
+++ src/parse_horarios.js
@@ -2,6 +2,7 @@ const csv = require('csv-parser');
 const path = require('path');
 const fs = require('fs');
 const db = require('./models');
+const moment = require('moment');
 
 const tiempo_periodos = {
     1: {inicio: "2023-09-11T09:00:00Z", fin: "2023-12-21T21:00:00Z"},
@@ -20,71 +21,130 @@ const dias_int = {
 
 async function parseHorarios(filename) {
     const results = [];
+    let keys = [];
     fs.createReadStream(path.join(__dirname, filename))
     .on('error', (error) => console.log("Error while trying to read file %s: %s", filename, error))
     .pipe(csv({separator: ';'}))
+    .on('headers', (headers) => keys = headers)
     .on('data', (data) => results.push(data))
     .on('end', async () => {
-        results.forEach(async (row) => {
-            // Elementos de la fila
-            const titulacion = row['Titulación'];
-            const curso = parseInt(row['Curso'].split('º')[0]); // "Númeroº" => Int Número
-            const codGEA = row['Cod.GEA'];
-            const asignatura_siglas = (row['Asignatura'].split(' ('));
-            const grupo = row['Grupo'];
-            const departamentos = (row['Departamento'].split('/'));
-            const periodo = row['Periodo'];
-            const docencia = row['Docencia'];
+        for (let i = 0; i < results.length; i++) {
+            
+            while (results[i][keys[7]].includes('UPM') || results[i][keys[7]].includes('IMDEA')) { // Recursión externa a la UCM
+                i++;
+            }
 
-            // Si Titulación no existe, se inserta
-            const entidad_titulacion = await db.sequelize.models.titulacion.findOrCreate({
+            const row = results[i];
+            // Elementos de la fila
+            const titulacion = row[keys[0]];
+            let curso = row[keys[1]];
+            const codGEA = row[keys[2]];
+            const asignatura_siglas = (row[keys[3]].split('('));
+            const grupo = row[keys[4]];
+            const departamentos = row[keys[5]];
+            const periodo = row[keys[6]];
+            const docencia = row[keys[7]];
+
+            //console.log(JSON.stringify(row) + '\n', titulacion, curso, codGEA, asignatura_siglas, grupo, departamentos, periodo, docencia);
+
+
+            // Traemos de la base de datos la titulación especificada
+            const entidad_titulacion = await db.sequelize.models.Titulacion.findOne({
                 where: {
-                    id: titulacion
-                },
-                defaults: { 
-                    id: titulacion
+                    siglas: titulacion
                 }
             });
 
-            // Curso y Grupo se aplican para hacer una instancia de la tabla Grupo
-            const entidad_grupo = await db.sequelize.models.grupo.findOrCreate({
+            // Traemos de la base de datos el plan de la titulación
+            // DE MOMENTO EL MÁS RECIENTE POR FALTA DE INFORMACIÓN
+            const entidad_plan = await db.sequelize.models.Plan.findOne({
                 where: {
-                    curso: curso,
-                    letra: grupo
+                    titulacion_id: entidad_titulacion.id
                 },
-                defaults: { 
-                    curso: curso,
-                    letra: grupo
-                }
+                order: ['creadoEn']
             });
 
             // Cod.GEA con Asignatura se aplican para hacer una instancia de la tabla Asignatura
             // El nombre de dicha instancia es la primera parte de Asignatura, y las siglas la segunda parte (entre paréntesis)
             // El departamento de dicha instancia es el extraido de la fila, lo mismo con el periodo
-            let nombre = asignatura_siglas[0];
+            let nombre = asignatura_siglas[0].trim();
             let siglas = asignatura_siglas[1].slice(0, asignatura_siglas[1].length - 1);
             
-            for (let i = 0; i < departamentos.length; i++) { // Preguntar a Iván si merece la pena, o mejor guardar el string directamente
-                let dep = departamentos[i];
-                await db.sequelize.models.asignatura.findOrCreate({
+            // Creamos la asignatura si no existe
+            const [entidad_asignatura, asignatura_cr] = await db.sequelize.models.Asignatura.findOrCreate({
+                where: {
+                    id: codGEA,
+                    nombre: nombre,
+                    siglas: siglas
+                },
+                defaults: { 
+                    id: codGEA,
+                    nombre: nombre,
+                    siglas: siglas,
+                    departamento: departamentos,
+                    periodo: periodo
+                }
+            });
+
+            if (!asignatura_cr) {
+                if (entidad_asignatura.dataValues.periodo != 'Anual' && entidad_asignatura.dataValues.periodo != periodo) {
+                    await db.sequelize.models.Asignatura.update({periodo: 'Anual'}, { where: {id: codGEA}} );
+                }
+                if (!entidad_asignatura.dataValues.departamento.includes(departamentos)) { // Re: preguntar a Iván como va a ser el formato de verdad
+                    if (departamentos.includes(entidad_asignatura.departamento)) {
+                        await db.sequelize.models.Asignatura.update({departamento: departamentos}, { where: {id: codGEA}} );
+                    }
+                    else {
+                        await db.sequelize.models.Asignatura.update({departamento: entidad_asignatura.dataValues.departamento + '/' + departamentos}, { where: {id: codGEA}} );
+                    }
+                }
+            }
+
+            await entidad_asignatura.setDe_plan(entidad_plan);
+
+            if (curso === "Optativas") { //El único caso en el que esto sucede es para ADE-GI, donde solo pone Optativas, sin un curso asociado
+                curso = '5';
+            }
+            let lista_cursos = (curso.match(/\d+/g)).map(num => parseInt(num));
+            let entidades_clase = [];
+            for (let j = 0; j < lista_cursos.length; j++) {
+                let curso = lista_cursos[j];
+                
+                // Curso y Grupo se aplican para hacer una instancia de la tabla Grupo
+                const [entidad_grupo, grupo_cr] = await db.sequelize.models.Grupo.findOrCreate({
                     where: {
-                        id: codGEA,
-                        nombre: nombre,
-                        siglas: siglas,
-                        departamento: dep,
-                        periodo: periodo
+                        curso: curso,
+                        letra: grupo
                     },
                     defaults: { 
-                        id: codGEA,
-                        nombre: nombre,
-                        siglas: siglas,
-                        departamento: dep,
-                        periodo: periodo
+                        curso: curso,
+                        letra: grupo
                     }
                 });
+
+
+                // Relacionar la asignatura con el grupo
+                if (!(await entidad_asignatura.hasPara_grupos(entidad_grupo))) {
+                    await entidad_asignatura.addPara_grupos(entidad_grupo);
+                }
+                
+                entidades_clase.push(await db.sequelize.models.Clase.findOne({
+                    where: {
+                        asignatura_id: entidad_asignatura.dataValues.id,
+                        grupo_id: entidad_grupo.dataValues.id
+                    }
+                }));
             }
             
-            // Docencia lleva mucho curro
+            // Docente responsable de la actividad 
+            // DE MOMENTO ALEATORIO POR FALTA DE INFORMACIÓN
+            const x = (i % await db.sequelize.models.Docente.count()) + 1;
+            const entidad_docente = await db.sequelize.models.Docente.findOne({
+                where: {
+                    id: x
+                }
+            });
+            
             // Tratar el string como expresión regular para analizar las distintas partes de este
             // (Espacios) => (Días):(Hora_inicio)-(Hora_fin) (| ((Espacios) =>)? (Días):(Hora_inicio)-(Hora_fin))*
             let actividades = docencia.split(' | ');
@@ -98,13 +158,14 @@ async function parseHorarios(filename) {
                 let actividad = actividades[j];
                 let aux = '';
 
-                if (actividad.contains('=> ')) {
-                    aux = actividad.split('=> ');
-                    espacios = aux[0];
-                    horarios = aux[1];
+                if (actividad.includes('=>')) {
+                    aux = actividad.split('=>');
+                    espacios = aux[0].trim();
+                    horarios = aux[1].trim();
                     aux = espacios.split(' ');
-                    tipo_esp = aux[0];
-                    num_esp = aux[1];
+                    tipo_esp = aux[0].trim();
+                    num_esp = aux[1].trim();
+                    //console.log(espacios, horarios, tipo_esp, num_esp);
                 }
                 else {
                     horarios = actividad;
@@ -112,13 +173,13 @@ async function parseHorarios(filename) {
 
                 let espacios_procesados = [];
 
-                if (num_esp.contains(',')) { // Separamos si hay más de un espacio
+                if (num_esp.includes(',')) { // Separamos si hay más de un espacio
                     num_esp = num_esp.split(',');
                     num_esp.forEach((elem) => {
                         let edificio = 'FdI';
                         let trimmed = elem.trim(); // Quitamos espacios redundantes
                         
-                        if (trimmed.contains('Mult.-')) { // Si es del estilo "Mult.-1208"
+                        if (trimmed.includes('Mult.-')) { // Si es del estilo "Mult.-1208"
                             trimmed = trimmed.slice(6);
                             edificio = 'Multiusos';
                         }
@@ -132,7 +193,7 @@ async function parseHorarios(filename) {
                     let edificio = 'FdI';
                     let trimmed = num_esp.trim(); // Quitamos espacios redundantes
                         
-                    if (trimmed.contains('Mult.-')) { // Si es del estilo "Mult.-1208", separamos la información
+                    if (trimmed.includes('Mult.-')) { // Si es del estilo "Mult.-1208", separamos la información
                         trimmed = trimmed.slice(6);
                         edificio = 'Multiusos';
                     }
@@ -143,40 +204,36 @@ async function parseHorarios(filename) {
                 }
 
                 // Separamos los días de las horas, y luego la hora de inicio con la hora de fin
-                let [dias, horas] = horarios.split(':', 1);
+                let split_point = horarios.indexOf(':');
+                let dias = horarios.substring(0, split_point);
+                let horas = horarios.substring(split_point + 1);
                 let [hora_inicio, hora_fin] = horas.split('-');
 
-                const actividad_resultante = await db.sequelize.models.actividad.findOrCreate({ //porqué findOrCreate??
-                    where: {
-                        fecha_inicio: tiempo_periodos[periodo].inicio,
-                        fecha_fin: tiempo_periodos[periodo].fin,
-                        tiempo_inicio: hora_inicio,
-                        tiempo_fin: hora_fin,
-                        es_todo_el_dia: 'No',
-                        es_recurrente: 'Sí',
-                        creadoPor: "CSVParser"
-                    },
-                    defaults: { 
-                        fecha_inicio: tiempo_periodos[periodo].inicio,
-                        fecha_fin: tiempo_periodos[periodo].fin,
-                        tiempo_inicio: hora_inicio,
-                        tiempo_fin: hora_fin,
-                        es_todo_el_dia: 'No',
-                        es_recurrente: 'Sí',
-                        creadoPor: "CSVParser" 
-                    }
+                const actividad_resultante = await db.sequelize.models.Actividad.create({
+                    fecha_inicio: tiempo_periodos[periodo].inicio,
+                    fecha_fin: tiempo_periodos[periodo].fin,
+                    tiempo_inicio: ( hora_inicio.includes(':') ? moment(hora_inicio, 'HH:mm').format('HH:mm') : moment(hora_inicio, 'HH').format('HH:mm') ),
+                    tiempo_fin: (hora_fin && hora_fin.includes(':') ? moment(hora_fin, 'HH:mm').format('HH:mm') : moment(hora_fin, 'HH').format('HH:mm') ),
+                    responsable_id: entidad_docente.id, //Tiene que ser declarado explícitamente en Sequelize v6
+                    es_todo_el_dia: 'No',
+                    es_recurrente: 'Sí',
+                    creadoPor: "CSVParser" 
                 });
 
                 for (let l = 0; l < dias.length; l++) { // Crear una recurrencia por cada dia de la semana que se repita la actividad
                     let dia = dias[l];
 
-                    await db.sequelize.models.recurrencia.create({dia_semana: dias_int[dia], actividad_id: actividad_resultante.id});
+                    await db.sequelize.models.Recurrencia.create({
+                        tipo_recurrencia: 'Semanal',
+                        dia_semana: dias_int[dia],
+                        actividad_id: actividad_resultante.id
+                    });
                 }
 
                 for (let k = 0; k < espacios_procesados.length; k++) { // Relacionar la actividad con cada espacio
                     let esp = espacios_procesados[k];
 
-                    let esp_entity = await db.sequelize.models.espacio.findOne({ //Solo hay uno, pues hay un constraint unique para (tipo, numero, edificio)
+                    let esp_entity = await db.sequelize.models.Espacio.findOne({ //Solo hay uno, pues hay un constraint unique para (tipo, numero, edificio)
                         attributes: ['id'],
                         where: {
                             tipo: esp.tipo,
@@ -186,19 +243,20 @@ async function parseHorarios(filename) {
                     });
 
                     // Relacionar la actividad con el espacio
-                    await actividad_resultante.addEspacio(esp_entity);
+                    await actividad_resultante.addImpartida_en(esp_entity);
 
+                }
+
+                // Crear la relación entre la clase de la asignatura y las actividades que dará dicha clase.
+                for (let k = 0; k < entidades_clase.length; k++) {   
+                    await entidades_clase[k].addCon_sesiones(actividad_resultante);
                 }
             
             }
-            // FALTA:
-            // Crear la clase que relaciona la asignatura con el grupo (entidad_asignatura.add<algo, no me acuerdo>(entidad_grupo))
-            // Crear la relación entre titulación y asignatura a través de plan, pero ni idea de como sacar qué plan corresponde.
-            // Preguntar si el plan se debe sacar como el último en creación que haga referencia a la titulación, asumiendo que la titulación ya está creada siempre.
-            // Crear la relación entre la clase de la asignatura y las actividades que dará dicha clase.
-
-        })
+        }
     });
 }
+
+//IMPORTANTE, caso 803289, AC, que en un único grupo se da en otro periodo, se toma como anual??
 
 parseHorarios('Horario2324.csv');
