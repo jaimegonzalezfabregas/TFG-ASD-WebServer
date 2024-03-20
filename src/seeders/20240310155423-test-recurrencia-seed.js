af5f36a715cb48f96deeebf8962d2535f3b10128
Helenan <you@example.com>
2024-03-20 19:49:58 +0100

    Añadidos datos de prueba de recurrencia
    Co-authored-by: jgaldoo <jgaldoo@users.noreply.github.com>

--- src/seeders/20240310155423-test-recurrencia-seed.js
+++ src/seeders/20240310155423-test-recurrencia-seed.js
@@ -12,6 +12,14 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    
+    return await queryInterface.bulkInsert('Recurrencia', [
+      { id: 1, actividad_id: 1, tipo_recurrencia: 'Semanal' , separacion: 0, dia_semana: 0},
+      { id: 2, actividad_id: 1, tipo_recurrencia: 'Semanal' , separacion: 0, dia_semana: 1},
+      { id: 3, actividad_id: 2, tipo_recurrencia: 'Semanal' , separacion: 0, dia_semana: 2},
+      { id: 4, actividad_id: 2, tipo_recurrencia: 'Semanal' , separacion: 0, dia_semana: 3},
+      { id: 5, actividad_id: 2, tipo_recurrencia: 'Semanal' , separacion: 0, dia_semana: 4}
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +29,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     return await queryInterface.bulkDelete('Recurrencia', null, {});
   }
 };
