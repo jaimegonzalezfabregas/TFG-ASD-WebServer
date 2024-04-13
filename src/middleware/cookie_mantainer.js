c0a9e6bcf5b3859cbfd4762b350ed84d1114480b
Helenan <you@example.com>
2024-04-13 14:05:50 +0200

    Añadida nueva cookie a mantener y quitados console.log
    Co-authored-by: jgaldoo <jgaldoo@users.noreply.github.com>

--- src/middleware/cookie_mantainer.js
+++ src/middleware/cookie_mantainer.js
@@ -1,18 +1,13 @@
-
-const keepAlways = ['id', 'email', 'nombre', 'apellidos', 'rol'];
+const keepAlways = ['id', 'email', 'nombre', 'apellidos', 'rol', 'offset'];
 
 //keepCookies(['espacio_id', 'actividad_id']) = Mantener vivas todas las cookies que sean espacio_id o actividad_id, o formen parte de keepAlways
 function keepCookies(req, res, cookie_list) { 
     let curr_cookies = req.session.user;
-    console.log(curr_cookies);
     for (cookie_key in curr_cookies) {
-        console.log(cookie_key);
         if (!(keepAlways.includes(cookie_key) || cookie_list.includes(cookie_key))) {
-            console.log('Deleting cookie ', cookie_key, ': ', req.session.user[cookie_key]);
             delete req.session.user[cookie_key];
         }
     }
-    console.log('FIN COOKIES');
     return true;
 }
 
