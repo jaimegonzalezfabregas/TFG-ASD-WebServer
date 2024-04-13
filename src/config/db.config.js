26af796dc20a501a9f35b091acee66231770e931
Helenan <you@example.com>
2024-04-13 13:07:43 +0200

    Cambio de archivo de config de la base de datos a js
    Co-authored-by: jgaldoo <jgaldoo@users.noreply.github.com>

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     development: {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_DEV_HOST,
-        port: process.env.DB_DEV_PORT,
-        database: process.env.DB_DEV_NAME,
+        dialect: (process.env.DB_DIALECT) ? process.env.DB_DIALECT : 'mysql',
+        host: (process.env.DB_DEV_HOST) ? process.env.DB_DEV_HOST : `localhost:${(process.env.DB_DEV_PORT) ? process.env.DB_DEV_PORT : 3306}`,
+        port: (process.env.DB_DEV_PORT) ? process.env.DB_DEV_PORT : 3306,
+        database: (process.env.DB_DEV_NAME) ? process.env.DB_DEV_NAME : "ASD_development_database",
         username: process.env.DB_DEV_USER,
         password: process.env.DB_DEV_PASS,
         dialectOptions: {
@@ -17,14 +17,14 @@ module.exports = {
               return next();
             },
         },
-        timezone: process.env.DB_TIMEZONE
+        timezone: "+00:00"
     },
     test: {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_TEST_HOST,
-        port: process.env.DB_TEST_PORT,
-        database: process.env.DB_TEST_NAME,
-        username: process.env.DB_TES_USER,
+      dialect: (process.env.DB_DIALECT) ? process.env.DB_DIALECT : 'mysql',
+      host: (process.env.DB_TEST_HOST) ? process.env.DB_TEST_HOST : `localhost:${(process.env.DB_TEST_PORT) ? process.env.DB_TEST_PORT : 3306}`,
+      port: (process.env.DB_TEST_PORT) ? process.env.DB_TEST_PORT : 3306,
+      database: (process.env.DB_TEST_NAME) ? process.env.DB_TEST_NAME : "ASD_test_database",
+        username: process.env.DB_TEST_USER,
         password: process.env.DB_TEST_PASS,
         dialectOptions: {
             dateStrings: true, // Pasar fechas a strings (para ignorar la zona horaria)
@@ -35,13 +35,13 @@ module.exports = {
               return next();
             },
         },
-        timezone: process.env.DB_TIMEZONE
+        timezone: "+00:00"
     },
     production: {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_PROD_HOST,
-        port: process.env.DB_PROD_PORT,
-        database: process.env.DB_PROD_NAME,
+        dialect: (process.env.DB_DIALECT) ? process.env.DB_DIALECT : 'mysql',
+        host: (process.env.DB_PROD_HOST) ? process.env.DB_PROD_HOST : `localhost:${(process.env.DB_PROD_PORT) ? process.env.DB_PROD_PORT : 3306}`,
+        port: (process.env.DB_PROD_PORT) ? process.env.DB_PROD_PORT : 3306,
+        database: (process.env.DB_PROD_NAME) ? process.env.DB_PROD_NAME : "ASD_production_database",
         username: process.env.DB_PROD_USER,
         password: process.env.DB_PROD_PASS,
         dialectOptions: {
@@ -53,7 +53,8 @@ module.exports = {
               return next();
             },
         },
-        timezone: process.env.DB_TIMEZONE
+        timezone: "+00:00",
+        logging: false
     }
 }
 
