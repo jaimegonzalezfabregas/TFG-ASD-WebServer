e72ad60a5a6655a965bc55768809b5af78eecb7e
Helenan <you@example.com>
2024-04-17 18:51:07 +0200

    Arreglado no_asistenciador para tener en cuenta excepciones, tiempos en UTC y arreglada expresion Cron
    Co-authored-by: jgaldoo <jgaldoo@users.noreply.github.com>

--- src/no_asistenciador.js
+++ src/no_asistenciador.js
@@ -7,7 +7,7 @@ const logger = require('./config/logger.config').child({"process": "asistencia_d
 
 async function noAsistenciar() {
     let num_no_asistidas = 0;
-    const current_date = moment(moment.now());
+    const current_date = moment().utc();
     
     const query_act = await db.sequelize.models.Actividad.findAll({
         attributes: ['id', 'tiempo_inicio', 'tiempo_fin', 'fecha_inicio', 'fecha_fin', 'es_recurrente']
@@ -17,7 +17,6 @@ async function noAsistenciar() {
 
         // Lista de actividades para las que haya que comprobar asistencias
         let actividades_del_dia = [];
-        let fechas_actividades = [];
         for (let i = 0; i < query_act.length; i++) {  
             let act = query_act[i];
 
@@ -32,18 +31,36 @@ async function noAsistenciar() {
 
                 let rec_list = [];
                 query_rec.forEach((elem) => rec_list.push(elem.dataValues));
-                let ultima_actividad = moment(recurrence_tool.getLastEventOfActividad(act, rec_list));
+                let [exists, ultima_actividad] = recurrence_tool.getLastEventOfActividad(act, rec_list);
 
-                if (ultima_actividad.isValid() && ultima_actividad.format('DD-MM-YYYY') == current_date.format('DD-MM-YYYY')) {
-                    actividades_del_dia.push(act.dataValues.id);
-                    fechas_actividades[act.dataValues.id] = {inicio: ultima_actividad, fin: recurrence_tool.getFinActividad(ultima_actividad, act.dataValues.tiempo_fin)};
+                if (exists && ultima_actividad.utc().format('DD-MM-YYYY') == current_date.format('DD-MM-YYYY')) {
+                    actividades_del_dia.push({
+                        id: act.dataValues.id,
+                        inicio: ultima_actividad.format('YYYY-MM-DD HH:mm:00'), 
+                        fin: recurrence_tool.getFinActividad(ultima_actividad, moment(act.dataValues.tiempo_fin, 'HH:mm').utc().format('HH:mm')).format('YYYY-MM-DD HH:mm:00')
+                    });
                 }
             }
             else if (act.dataValues.fecha_fin && act.dataValues.fecha_fin.includes(current_date.format('DD-MM-YYYY'))) { // Termina en el día
-                actividades_del_dia.push(act.dataValues.id);
-                fechas_actividades[act.dataValues.id] = {inicio: act.dataValues.fecha_inicio, fin: act.dataValues.fecha_fin};
+                actividades_del_dia.push({id: act.dataValues.id, inicio: act.dataValues.fecha_inicio, fin: act.dataValues.fecha_fin});
             }
         };
+
+        // Reprogramaciones para hoy
+        const reprogramaciones = await db.sequelize.models.Excepcion.findAll({
+            attributes: ['actividad_id', 'fecha_inicio_ex', 'fecha_fin_ex'],
+            where: {
+                fecha_fin_ex: { [Op.and]: [{ [Op.gte]: moment().utc().format('YYYY-MM-DDT00:00:00') }, { [Op.lte]: moment().utc().format('YYYY-MM-DDT23:59:59') }]},
+                esta_reprogramado: 'Sí',
+                esta_cancelado: 'No'
+            }
+        });
+
+        reprogramaciones.forEach((rep => {
+            actividades_del_dia.push({id: rep.dataValues.actividad_id, inicio: rep.dataValues.fecha_inicio_ex + 'Z', fin: rep.dataValues.fecha_fin_ex + 'Z'});
+        }));
+
+        console.log(actividades_del_dia);
 
         for(let i = 0; i < actividades_del_dia.length; i++) {
             // El responsable de la actividad da clase en ella, así que está en la relación imparte
@@ -53,7 +70,7 @@ async function noAsistenciar() {
                     model: db.sequelize.models.Actividad,
                     as: 'imparte',
                     where: {
-                        id: actividades_del_dia[i]
+                        id: actividades_del_dia[i].id
                     }
                 }
             });
@@ -64,7 +81,7 @@ async function noAsistenciar() {
                     model: db.sequelize.models.Actividad,
                     as: 'ocupado_por',
                     where: {
-                        id: actividades_del_dia[i]
+                        id: actividades_del_dia[i].id
                     }
                 }
             });
@@ -78,7 +95,7 @@ async function noAsistenciar() {
 
             query_esp.forEach((esp) => {
                 lista_espacios.push(esp.dataValues.id);
-            })
+            });
             
             // Buscar asistencias de las actividades
             // Para tener en cuenta actividades que duren más de un día (si es que existen), buscar en un intervalo (where: { fecha: [Op.and]: [[Op.gt] inicio, [Op.lt]: fin]]})
@@ -94,13 +111,13 @@ async function noAsistenciar() {
                             [Op.or]: lista_espacios
                         },
                         fecha: {
-                            [Op.and]: [{[Op.gte]: fechas_actividades[actividades_del_dia[i]].inicio}, {[Op.lte]: fechas_actividades[actividades_del_dia[i]].fin}]
+                            [Op.and]: [{[Op.gte]: actividades_del_dia[i].inicio}, {[Op.lte]: actividades_del_dia[i].fin}]
                         }
                     },
                     defaults: {
                         espacio_id: lista_espacios[0],
                         docente_id: docente_actual,
-                        fecha: fechas_actividades[actividades_del_dia[i]].inicio,
+                        fecha: actividades_del_dia[i].inicio,
                         estado: 'No Asistida'
                     }   
                 });
@@ -112,11 +129,11 @@ async function noAsistenciar() {
         }
     }
     
-    logger.info(num_no_asistidas, 'no asistencias registradas');
+    logger.info(`${num_no_asistidas} no asistencias registradas`);
 }
 
 // Expresiones de 6, * para no especificar, en ss mm HH DD MM nD (nD = número de días)
-// Para que funcione bien, ejecutar en el MISMO día (sobre las 23h, '23 * * *')
-Cron('* 23 * * *', noAsistenciar());
+// Para que funcione bien, ejecutar en el MISMO día (Todos los días, a las 23:00:00 = '0 0 23 * *')
+Cron('0 0 23 * *', noAsistenciar());
 
 //noAsistenciar();
