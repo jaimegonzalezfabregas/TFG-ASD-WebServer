988d7ca468d6d28c6f6dcfc7a5a98b8951a4bff3
Helenan <you@example.com>
2024-04-17 19:21:53 +0200

    Cambiado valor por defecto de api_path
    Co-authored-by: jgaldoo <jgaldoo@users.noreply.github.com>

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -5,7 +5,7 @@ let host = (process.env.API_HOST) ? process.env.API_HOST : 'localhost';
 module.exports = {
     host: (process.env.API_HOST) ? process.env.API_HOST : 'localhost',
     port: (process.env.API_PORT) ? process.env.API_PORT : 3001,
-    path: (process.env.API_PATH) ? process.env.API_PATH : '',
+    path: (process.env.API_PATH) ? process.env.API_PATH : '/api',
     protocol: (process.env.API_PROTOCOL) ? process.env.API_PROTOCOL : "http",
     port_spec: (process.env.API_PORT_SPECIFICATION === "true" || host == 'localhost') ? true : false,
     secrets: (process.env.API_ALLOWED) ? JSON.parse(process.env.API_ALLOWED) : ["",""] 
